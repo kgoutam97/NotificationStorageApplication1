4d0ea63cfb3a228829eb688807557a86cb19d49d
kgoutam97 <goutamkeswani@gmail.com>
2021-04-30 16:39:47 +0530

    AAAAAAAAAA

--- src/main/java/com/decipher/NotificationStorageApplication.java
+++ src/main/java/com/decipher/NotificationStorageApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NotificationStorageApplication {
 
 	public static void main(String[] args) {
+     // this is for information purpose
 		SpringApplication.run(NotificationStorageApplication.class, args);
 	}
 	
